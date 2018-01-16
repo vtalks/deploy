26d524f92e84ed62bcdfe03ffa714fa533f46b37
Raül Pérez <repejota@gmail.com>
2018-01-16 18:11:17 +0100

    environment script template

---
+++ environment.dist.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# virtualenv
+source ../.venv/bin/activate
+
+# Twitter
+export TWITTER_TOKEN=''
+export TWITTER_SECRET=''
+# Youtube
+export YOUTUBE_API_KEY=''
+# Coveralls
+export COVERALLS_REPO_TOKEN=''
+
+# Aliases
+alias compose="docker-compose -f docker-compose.yml"
+alias manage="docker-compose -f docker-compose.yml exec web python3 manage.py"
