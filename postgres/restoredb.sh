c4c6356be7e1391237e288e9ba0561990acae455
Raül Pérez <repejota@gmail.com>
2018-01-23 03:14:42 +0100

    Better backup scripts

--- postgres/restoredb.sh
+++ postgres/restoredb.sh
@@ -5,4 +5,6 @@ set -e
 
 dropdb -U postgres -e --if-exists postgres && \
 createdb -U postgres postgres && \
-pg_restore -U postgres --dbname=postgres --clean --no-owner --no-privileges --verbose --jobs=2 /backup/database.snapshot.sql
+cat /backup/database.snapshot.sql | psql -U postgres
+
+# pg_restore -U postgres --dbname=postgres --clean --no-owner --no-privileges --verbose --jobs=2 /backup/database.snapshot.sql
