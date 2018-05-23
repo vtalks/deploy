fa4385080337c67344db277ecee3ccfe45e513d2
Raül Pérez <repejota@gmail.com>
2018-05-23 22:48:06 +0200

    Better scripts to backup and restore the database

--- postgres/dumpdb.sh
+++ postgres/dumpdb.sh
@@ -3,6 +3,4 @@
 # For use on the postgres container only.
 set -e
 
-# pg_dump -U postgres --compress=9 --no-privileges --no-owner --format=custom --file=/.backup/database.snapshot.sql postgres
-
 pg_dump -U postgres postgres > /.backup/database.snapshot.sql
--- postgres/restoredb.sh
+++ postgres/restoredb.sh
@@ -6,5 +6,3 @@ set -e
 dropdb -U postgres -e --if-exists postgres && \
 createdb -U postgres postgres && \
 cat /.backup/database.snapshot.sql | psql -U postgres
-
-# pg_restore -U postgres --dbname=postgres --clean --no-owner --no-privileges --verbose --jobs=2 /.backup/database.snapshot.sql
