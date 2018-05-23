bf23a055f4820e69300d79dafe62ae13688cc32f
Raül Pérez <repejota@gmail.com>
2018-05-23 21:31:22 +0200

    New environment.dist with all variables

--- environment.dist.sh
+++ environment.dist.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
 # Use virtualenv
-# source ../.venv/bin/activate
+source ../.venv/bin/activate
 
-# Use a docker machine (remote host)
-# eval $(docker-machine env vtalks)
+# Django configuration
+export DJANGO_SETTINGS_MODULE='settings.dev'
 
-# Unset docker machine configuration
-# unset DOCKER_TLS_VERIFY
-# unset DOCKER_HOST
-# unset DOCKER_CERT_PATH
-# unset DOCKER_MACHINE_NAME
+# Mailgun
+export MAILGUN_API_KEY=''
+export MAILGUN_SMTP_PASSWORD=''
+export MAILGUN_SMTP_USER='postmaster@mg.vtalks.net'
+
+# Twitter (to publish)
+export TLKSIO_TWITTER_TOKEN=""
+export TLKSIO_TWITTER_SECRET=""
+export TLKSIO_TWITTER_ACCESS_TOKEN=""
+export TLKSIO_TWITTER_ACCESS_SECRET=""
 
 # Twitter
 export TWITTER_TOKEN=''
 export TWITTER_SECRET=''
 # Youtube
 export YOUTUBE_API_KEY=''
-# Coveralls
+# Coveralls
 export COVERALLS_REPO_TOKEN=''
 
 # Aliases
-alias compose="docker-compose -f docker-compose.yml"
+# alias compose="docker-compose -f docker-compose.yml -f docker-compose-dev.yml"
+# alias compose="docker-compose -f docker-compose.yml -f docker-compose-prod.yml"
 alias manage="compose exec web python3 manage.py"
