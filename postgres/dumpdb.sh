8d65066f17c10daab122473d4232262f7c696412
Raül Pérez <repejota@gmail.com>
2018-06-02 16:19:21 +0200

    New dumpdb postgres script

--- postgres/dumpdb.sh
+++ postgres/dumpdb.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+TIMESTAMP=`date '+%Y%m%d%H%M%S'`
+
 # For use on the postgres container only.
 set -e
 
 pg_dump -U postgres postgres > /.backup/database.snapshot.sql
+cp /.backup/database.snapshot.sql /.backup/database.snapshot.${TIMESTAMP}.sql
+gzip /.backup/database.snapshot.${TIMESTAMP}.sql
