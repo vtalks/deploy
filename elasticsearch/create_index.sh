26e227c0c27bca580076a475efa89fcab45edb96
Raül Pérez <repejota@gmail.com>
2018-06-24 18:39:40 +0200

    delete index

--- elasticsearch/create_index.sh
+++ elasticsearch/create_index.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
 
-curl -X PUT "localhost:9200/talk?pretty"
+curl -X PUT "localhost:9200/talk" -H 'Content-Type: application/json' -d'
+{
+    "settings" : {
+        "index" : {
+            "number_of_shards" : 1,
+            "number_of_replicas" : 0
+        }
+    }
+}
+'
