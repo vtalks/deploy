51d227c84b7ddf511cc7936e352bd59f5a0a4878
Raül Pérez <repejota@gmail.com>
2018-06-29 00:46:01 +0200

    better mappings

--- elasticsearch/create_index.sh
+++ elasticsearch/create_index.sh
@@ -7,6 +7,24 @@ curl -X PUT "localhost:9200/vtalks" -H 'Content-Type: application/json' -d'
             "number_of_shards" : 1,
             "number_of_replicas" : 0
         }
+    },
+    "mappings" : {
+        "talk" : {
+            "properties" : {
+                "created" : {
+                    "type" : "date"
+                },
+                "updated" : {
+                    "type" : "date"
+                },
+                "wilsonscore_rank": {
+                    "type": "float"
+                },
+                "hacker_hot": {
+                    "type": "float"
+                }
+            }
+        }
     }
 }
 '
